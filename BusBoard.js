b4b38deb590564285e387a781b68470f5b98ed1f
Noorulain Fahad <Noorulain.Fahad@softwire.com>
2022-08-12 19:57:04 +0100

    Getting Bus Arrivals-Async

--- BusBoard.js
+++ BusBoard.js
@@ -1,15 +1,15 @@
-//const fetch = require('node-fetch')
+
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const readline = require('readline-sync')
 
-function getNextBusStops(){
+async function getNextBusArrivals(){
 
-    console.log('Please enter Bus Stop code: '); 
-    BusStopCode = readline.prompt();
-    const NextBusStops = fetch('https://api.tfl.gov.uk/StopPoint/'+BusStopCode+'/Arrivals') 
-    .then(response => response.json())
-    .then(body => console.log(body));
-
+  console.log('Please enter the Bus Stop Code: '); 
+  const BusStopCode = readline.prompt();
+      
+  const NextBusArrivalsResponse = await fetch('https://api.tfl.gov.uk/StopPoint/'+BusStopCode+'/Arrivals') 
+  const NextBusArrivalsData = await NextBusArrivalsResponse.json(); 
+  console.log(NextBusArrivalsData);
+                        
 }
-
-getNextBusStops();
+getNextBusArrivals();
